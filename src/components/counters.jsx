8c03d89989b82d5abed7c2b9b90debe33362f8b8
gwillig <gustavwillig@gmail.com>
2019-05-07 21:58:40 +0200

    Add style

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -34,9 +34,19 @@ class Counters extends Component{
     counters.map(c=>c.value=0);
     this.setState({counters});
   };
+  setSlider = () => {
+
+    setTimeout(function(){
+      console.log("asd")
+    document.querySelector("#cover").style.zIndex=12
+    document.querySelector("#message").style.color="black"
+  },3000)
+  }
   render(){
-    return (<div className="container">
-            <div id="Cover" onLoad={console.log("hello")}></div>
+    return (<div id="container_counters" className="container">
+            <div id="cover" onLoad={this.setSlider()}>
+                <p id="message">Thanks for your help</p>
+            </div>
               <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
               <div className="row">
               {this.state.counters.map(counter =>(
