59086c187f689cc54526553dd1ce16f428e53192
gwillig <gustavwillig@gmail.com>
2019-05-08 20:57:04 +0200

    Adjust model

--- research/restapi/admin.py
+++ research/restapi/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 from restapi.models import Choice
 # Register your models here.
+
+admin.site.register(Choice)
--- research/restapi/views.py
+++ research/restapi/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import datetime
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from restapi.models import Choice
+
+
+@csrf_exempt
+def result(request):
+    print(request)
+    newResponse = Choice(imag_1=1,imag_2=2,imag_3=3,imag_4=3,date=datetime.datetime.now())
+    newResponse.save()
+    response = {'newTheme': "theme"}
+
+    return JsonResponse(response)
