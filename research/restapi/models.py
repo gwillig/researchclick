506017f4832b3e78bdc3f38dd42b61219694cbf1
gwillig <gustavwillig@gmail.com>
2019-05-08 20:57:04 +0200

    Adjust model

--- research/restapi/models.py
+++ research/restapi/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
 class Choice(models.Model):
     imag_1 = models.IntegerField(default=0)
     imag_2 = models.IntegerField(default=0)
     imag_3 = models.IntegerField(default=0)
     imag_4 = models.IntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now, blank=True, null=True)
+
+    def __str__(self):
+       return str(self.date)
