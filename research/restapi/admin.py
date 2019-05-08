28da4faac446a6e4fbc32a418010d5639e97e38a
gwillig <gustavwillig@gmail.com>
2019-05-08 19:42:48 +0200

    Add new django model to save the findings

--- research/restapi/admin.py
+++ research/restapi/admin.py
@@ -1,3 +1,3 @@
 from django.contrib import admin
-
+from restapi.models import Choice
 # Register your models here.
--- research/restapi/models.py
+++ research/restapi/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Choice(models.Model):
+    imag_1 = models.IntegerField(default=0)
+    imag_2 = models.IntegerField(default=0)
+    imag_3 = models.IntegerField(default=0)
+    imag_4 = models.IntegerField(default=0)
