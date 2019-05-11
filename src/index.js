9bfaf8d5290564c9a9cae2af2b4996342d07e226
gwillig <gustavwillig@gmail.com>
2019-05-11 14:09:27 +0200

    - Add first impression of the first participant. - Add a todo list based on the feedback of the participant.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Counters from "./components/counters"
-import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.render(<Counters/>, document.getElementById('root'));
 
