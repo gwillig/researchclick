dc24e15afa6423a1646779bb2f18c9daeb798c65
gwillig <gustavwillig@gmail.com>
2019-05-12 17:03:26 +0200

    Add new field for mood in databank

--- restapi/views.py
+++ restapi/views.py
@@ -21,13 +21,14 @@ def result(request):
     print(time_now)
     #3.Step: Create new object to save the answers
     newResponse = Choice(
-        imag_1=ds["0"][0],imag_1_url=ds["0"][1],
-        imag_2=ds["1"][0],imag_2_url=ds["1"][1],
-        imag_3=ds["2"][0], imag_3_url=ds["2"][1],
-        imag_4=ds["3"][0], imag_4_url=ds["3"][1],
-        imag_5=ds["4"][0], imag_5_url=ds["4"][1],
-        imag_6=ds["5"][0], imag_6_url=ds["5"][1],
-        date=time_now)
+        imag_1 = ds["0"][0],imag_1_url=ds["0"][1],
+        imag_2 = ds["1"][0],imag_2_url=ds["1"][1],
+        imag_3 = ds["2"][0], imag_3_url=ds["2"][1],
+        imag_4 = ds["3"][0], imag_4_url=ds["3"][1],
+        imag_5 = ds["4"][0], imag_5_url=ds["4"][1],
+        imag_6 = ds["5"][0], imag_6_url=ds["5"][1],
+        mood   = ds["value"],
+        date   = time_now)
     newResponse.save()
     print("11Fetch successfully saved to databank")
     response = {'Repsonse': "Answer successfully saved to databank"}
