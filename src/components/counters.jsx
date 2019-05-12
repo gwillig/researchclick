ab3fa5638102e3c16399f4b7e289519f82fec291
gwillig <gustavwillig@gmail.com>
2019-05-12 18:40:35 +0200

    Created own coundown function

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import Counter from './counter'
 import Dropdown from './dropdown'
-import Countdown from 'react-countdown-now';
+
 
 class Counters extends Component{
   state = {
@@ -45,7 +45,6 @@ class Counters extends Component{
     return (<div id="container_counters" className="container">
               <Dropdown></Dropdown>
               <button onClick={this.handleReset} className="btn btn-dark btn-sm m-2">Reset</button>
-              <Countdown date={Date.now() + 25000}precision={3} /> {/*style is direct change for all span  tags in index.css*/}
               <div className="row">
               {this.state.counters.map(counter =>(
                 <Counter
