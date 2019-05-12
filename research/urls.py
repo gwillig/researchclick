ce0acf6584d779afa3086c26f110497849806315
gwillig <gustavwillig@gmail.com>
2019-05-12 09:17:50 +0200

    fix bug

--- research/urls.py
+++ research/urls.py
@@ -20,6 +20,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('response/', include('restapi.urls')),
+    path('response', include('restapi.urls')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
--- restapi/urls.py
+++ restapi/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('response/', views.result, name='result'),
+    path('response', views.result, name='result'),
 ]
