6b56e8441e8f51e66a4382fed4d992862eab0adf
gwillig <gustavwillig@gmail.com>
2019-05-12 09:13:01 +0200

    fix bug

--- restapi/views.py
+++ restapi/views.py
@@ -6,7 +6,7 @@ import json
 from restapi.models import Choice
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_http_methods
 # @method_decorator(csrf_exempt)
 # @csrf_exempt
 @require_http_methods(['POST'])
