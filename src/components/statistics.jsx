3e31b6f6996281eebfb2cc2b4d874e8d35656e4b
gwillig <gustavwillig@gmail.com>
2019-05-21 19:53:00 +0100

    add line chart

--- src/components/statistics.jsx
+++ src/components/statistics.jsx
@@ -1,10 +1,30 @@
 import React, {Component} from "react";
+import { render } from 'react-dom'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+
+const options = {
+  title: {
+    text: 'My chart'
+  },
+  series: [{
+    data: [1, 2, 3]
+  }]
+}
+
+const App = () => <div>
+  <HighchartsReact
+    highcharts={Highcharts}
+    options={options}
+  />
+</div>
 
 class Statistic extends Component{
   /*@description
   @args
-  */  
-
+  */
+render(){
+  return(<App />, document.getElementById('root'))}
 }
 
 export default Statistic;
