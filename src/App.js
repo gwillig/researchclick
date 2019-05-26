25077bc8e37644ce5ec4482f350e057720f70b8f
gwillig <gustavwillig@gmail.com>
2019-05-26 13:22:47 +0200

    fixed bug

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Welcome from './components/welcome';
+import Counters from './components/counters';
+
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+
+          <Switch>
+              <Route exact path='/' component={Welcome} />
+              <Route path='/research' component={Counters} />
+          </Switch>
+
+      </Router>
+    );
+  }
+}
+
+export default App;
