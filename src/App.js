0128783e08cfa68bb0df1cce32fe3d10bfdb408d
gwillig <gustavwillig@gmail.com>
2019-06-04 18:48:24 +0100

    Change style statistic site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Welcome from './components/welcome';
 import Counters from './components/counters';
+import Statistic from './components/statistics';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
           <Switch>
               <Route exact path='/' component={Welcome} />
               <Route path='/research' component={Counters} />
+              <Route path='/statistic' component={Statistic} />
           </Switch>
 
       </Router>
