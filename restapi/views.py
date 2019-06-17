a980b0e16bfb26ed215d6093d7925b0ac2cbd3a7
gwillig <gustavwillig@gmail.com>
2019-06-17 18:19:10 +0000

    adjust restapi getdata to get the data for a line chart

--- restapi/views.py
+++ restapi/views.py
@@ -9,23 +9,35 @@ from django.utils.decorators import method_decorator
 from datetime import datetime
 from django.http import HttpResponse
 from pytz import timezone
+from django.db.models.functions import TruncDay
+from django.db.models import Count
+
+
+
 
 @csrf_exempt
 def getdata(request):
     data_dict={}
     data_dict["all_records"] = Choice.objects.all().count()
+    ######## Create data for moods
 	#1.Step: Get all unique moods
     moods_queryset = Choice.objects.values('mood').distinct()
 	#2.Step: count
     mood_list =  moods_queryset.values_list('mood', flat=True)
-
-
-
     data_dict["pieChart"]= []
     for element in mood_list:
         value_element = Choice.objects.filter(mood=element).count()
         #Convert the response so that the response  is ready for highchart pie chart
         data_dict["pieChart"].append({"name": element,"y": value_element})
+
+    ####### Create data for records_per_day
+    records_per_day = Choice.objects.annotate(day=TruncDay('date')).values('day').annotate(records=Count('id'))
+    proc_records_per_day=[]
+    for exp in records_per_day:
+        proc_records_per_day.append([exp['day'].timestamp()*1000,exp['records']])
+
+    data_dict["lineChart"]= proc_records_per_day
+
 
     return JsonResponse(data_dict)
 
